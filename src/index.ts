d09134d9656a3236def306f6e0aad1a61f87daf0
Alex Kravchenko <alexkravchenko1605@gmail.com>
2023-08-04 00:03:54 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { file } from './scripts/first';
+const nameL = 'alex';
+
+const ale = 1;
+
+console.log('hello from typescript');
+console.log(ale);
+console.log(nameL);
+console.log(file);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'script.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
